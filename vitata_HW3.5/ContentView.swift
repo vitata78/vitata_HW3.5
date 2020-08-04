bcfcfc313d15be450299dfe36a040e59136a7a3a
Andrew Tolstov <andrew.tolstov@gmail.com>
2020-08-04 17:59:03 -0400

    Initial Commit

---
+++ vitata_HW3.5/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  vitata_HW3.5
+//
+//  Created by Andrew Tolstov on 8/4/20.
+//  Copyright © 2020 APNET HQ LLC. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
